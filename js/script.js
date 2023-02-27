9f5c2124f38302ec39fc75974f17373025b7aeb8
lucacappello79 <postt79@gmail.com>
2023-02-27 12:35:20 +0100

    init exercise

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Definisci un array di Email autorizzate a fare il login
+// Chiedi la mail all'utente e falla inviare con un input+button
+// dopo che l'utente ha cliccato e inviato i dati, controilla che la mail sia presente nell'elenco autorizzati
+// ?SE presente, stampare un messaggio adeguato
+
+
+const mailAuthorized = ["luca@gmail.com", "paolo@gmail.com", "pietro@gmail.com", "marco@gmail.com"]
+
+let inputMail = document.getElementById("userMail");
+let logInButton = document.getElementById("logIn");
+
+logInButton.addEventListener("click", function() {
+    if (mailAuthorized.includes(logInButton.value))
+    console.log
+}
